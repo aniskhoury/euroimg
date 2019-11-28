ef48a100b6a29c5b05e5980916876ef6fa1e4fbf
Anïs Khoury <akhouryribas@gmail.com>
2019-11-28 13:39:24 +0100

    Add files via upload

---
+++ sobreeuroimg.php
@@ -0,0 +1,52 @@
+<?php 
+/*  #######################################################
+	##                 Pàgina sobre EuroIMG              ##
+	#######################################################
+	##               Autor: Anïs Khoury Ribas            ##
+	##               Per Meinsa Sistemas S.L.            ##
+	##                      25/11/2019                   ##
+	#######################################################
+	
+	
+*/
+/*Carreguem els fragments de la pàgina web, com la capcelera, navegació, etc*/
+
+include("fragments.php");
+include("missatges.php");
+include("configuracio.php");
+?>
+
+<!DOCTYPE html>
+<html lang="<?php echo $llengua;?>">
+
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title><?php echo $missatges[$codiLlengua]["titolIndex"]; ?></title>
+  <!-- Custom fonts for this theme -->
+  <?php echo fragmentEstilIScripts(); ?>
+
+	<style>
+      #uploader {
+        width: 300px;
+        height: 200px; 
+        background: #f4b342; 
+        padding: 10px;
+      }
+	</style>
+</head>
+
+<body>
+	<?php echo fragmentNavegador($codiLlengua); ?>
+  <header class="masthead bg-primary text-white text-center">
+	<center>
+		Sobre EuroIMG - Falta fragment explicació sobre què és EuroIMG  
+	</center>
+   <br><br> 
+  </header>
+<?php echo fragmentPeuPagina($codiLlengua); ?>
+<?php echo fragmentAbaixDeTot($codiLlengua); ?>
+</body>	
